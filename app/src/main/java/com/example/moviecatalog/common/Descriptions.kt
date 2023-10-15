b81ed65dd64d715f946a537ac3762829bbe34186
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2023-10-15 12:01:03 +0700

    Добавлены объекты Constants и Descriptions
    Данные объекты необходимы для хранения константных строк и сообщений в приложении

---
+++ app/src/main/java/com/example/moviecatalog/common/Descriptions.kt
@@ -0,0 +1,15 @@
+package com.example.moviecatalog.common
+
+object Descriptions {
+    const val NEED_REGISTER = "Еще нет аккаунта?"
+    const val NEED_REGISTER_CLICKABLE = " Зарегистрируйтесь"
+
+    const val NEED_LOGIN = "Уже есть аккаунт?"
+    const val NEED_LOGIN_CLICKABLE = " Войдите"
+
+    const val AUTH_DESCRIPTION_FIRST = "Погрузись в мир кино"
+    const val AUTH_DESCRIPTION_SECOND =
+        "Мы предлагаем удобный и легкий способ " +
+                "насладиться любимыми фильмами прямо " +
+                "с Вашего мобильного устройства."
+}
