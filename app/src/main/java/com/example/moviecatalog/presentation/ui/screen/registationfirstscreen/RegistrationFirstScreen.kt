62af1176b58be4fb3f0432f5b619b1d444eaa6ff
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2023-10-15 13:24:15 +0700

    Добавлен первый экран регистрации

--- app/src/main/java/com/example/moviecatalog/presentation/ui/screen/registationfirstscreen/RegistrationFirstScreen.kt
+++ app/src/main/java/com/example/moviecatalog/presentation/ui/screen/registationfirstscreen/RegistrationFirstScreen.kt
@@ -1,8 +1,262 @@
 package com.example.moviecatalog.presentation.ui.screen.registationfirstscreen
 
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.moviecatalog.common.Constants
+import com.example.moviecatalog.common.Descriptions
+import com.example.moviecatalog.presentation.ui.screen.registationfirstscreen.components.DatePickerField
+import com.example.moviecatalog.presentation.ui.screen.registationfirstscreen.components.GenderSelectionButton
+import com.example.moviecatalog.presentation.ui.theme.AccentColor
+import com.example.moviecatalog.presentation.ui.theme.spanStyleAccent
+import com.example.moviecatalog.presentation.ui.theme.spanStyleGray
 
 @Composable
 fun RegistrationFirstScreen() {
+    val focusManager = LocalFocusManager.current
+    val context = LocalContext.current
+    var isDatePickerVisible by remember { mutableStateOf(false) }
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    focusManager.clearFocus()
+                })
+            },
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier
+                    .wrapContentSize(Alignment.Center)
+                    .align(alignment = Alignment.Center)
+            ) {
+                Text(
+                    text = Constants.LOGO,
+                    style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold),
+                    color = AccentColor
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .wrapContentSize(Alignment.CenterStart)
+                    .align(alignment = Alignment.CenterStart)
+            ) {
+                IconButton(
+                    onClick = {  },
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.KeyboardArrowLeft,
+                        contentDescription = "Back"
+                    )
+                }
+            }
+        }
+
+        Text(
+            text = Constants.REGISTRATION,
+            style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 16.dp, bottom = 16.dp)
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(
+                    text = Constants.NAME,
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {  },
+                    singleLine = true,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp),
+                    shape = RoundedCornerShape(10.dp),
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(
+                    text = Constants.GENDER,
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+                GenderSelectionButton()
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(
+                    text = Constants.LOGIN,
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    singleLine = true,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp),
+                    shape = RoundedCornerShape(10.dp),
+                )
+            }
+        }
+
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(
+                    text = Constants.EMAIL,
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    singleLine = true,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp),
+                    shape = RoundedCornerShape(10.dp),
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(
+                    text = Constants.DATE_OF_BIRTHDAY,
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+
+                DatePickerField()
+            }
+        }
+
+        Button(
+            onClick = { },
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 16.dp, start = 8.dp, end = 8.dp)
+                .height(IntrinsicSize.Min)
+        ) {
+            Text(
+                text = Constants.CONTINUE
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .wrapContentSize(Alignment.BottomCenter)
+                .padding(16.dp),
+        ){
+            val highlightedText = buildAnnotatedString {
+                withStyle(style = spanStyleGray){
+                    append(Descriptions.NEED_LOGIN)
+                }
+
+                withStyle(style = spanStyleAccent) {
+                    append(Descriptions.NEED_LOGIN_CLICKABLE)
+                }
+            }
+
+            ClickableText(
+                onClick ={ offset ->
+                    if (offset >= 16){
+
+                    }
+                },
+                text = highlightedText
+            )
+        }
+    }
 }
