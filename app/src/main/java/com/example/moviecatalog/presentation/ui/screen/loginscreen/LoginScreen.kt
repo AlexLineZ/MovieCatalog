c687bb709307882bee57a9b9a0a827d03c85d273
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2023-10-15 13:57:49 +0700

    Добавлен экран входа в аккаунт

--- app/src/main/java/com/example/moviecatalog/presentation/ui/screen/loginscreen/LoginScreen.kt
+++ app/src/main/java/com/example/moviecatalog/presentation/ui/screen/loginscreen/LoginScreen.kt
@@ -1,8 +1,213 @@
 package com.example.moviecatalog.presentation.ui.screen.loginscreen
 
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.moviecatalog.common.Constants
+import com.example.moviecatalog.common.Descriptions
+import com.example.moviecatalog.presentation.navigation.Destinations
+import com.example.moviecatalog.presentation.ui.theme.AccentColor
+import com.example.moviecatalog.presentation.ui.theme.spanStyleAccent
+import com.example.moviecatalog.presentation.ui.theme.spanStyleGray
 
 @Composable
-fun LoginScreen() {
+fun LoginScreen(navController: NavController) {
+    val focusManager = LocalFocusManager.current
+    var isPasswordVisible by remember { mutableStateOf(false) }
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    focusManager.clearFocus()
+                })
+            },
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier
+                    .wrapContentSize(Alignment.Center)
+                    .align(alignment = Alignment.Center)
+            ) {
+                Text(
+                    text = Constants.LOGO,
+                    style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold),
+                    color = AccentColor
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .wrapContentSize(Alignment.CenterStart)
+                    .align(alignment = Alignment.CenterStart)
+            ) {
+                IconButton(
+                    onClick = { navController.navigate("both") },
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.KeyboardArrowLeft,
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+
+        Text(
+            text = Constants.LOGIN_TO,
+            style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 16.dp, bottom = 16.dp)
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(
+                    text = Constants.LOGIN,
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    singleLine = true,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp)
+                        .height(IntrinsicSize.Min),
+                    shape = RoundedCornerShape(10.dp),
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Text(
+                    text = Constants.PASSWORD,
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+
+                )
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    singleLine = true,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp)
+                        .height(IntrinsicSize.Min),
+                    shape = RoundedCornerShape(10.dp),
+                    trailingIcon = {
+                        IconButton(
+                            onClick = {
+                                isPasswordVisible = !isPasswordVisible
+                            }
+                        ) {
+                            Icon(
+                                imageVector = if (isPasswordVisible) Icons.Default.Visibility
+                                else Icons.Default.VisibilityOff,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                )
+            }
+        }
+
+        Button(
+            onClick = { },
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 16.dp, start = 8.dp, end = 8.dp)
+                .height(IntrinsicSize.Min)
+        ) {
+            Text(
+                text = Constants.LOGIN_TO_BUTTON
+            )
+        }
+
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .wrapContentSize(Alignment.BottomCenter)
+                .padding(16.dp),
+        ){
+            val highlightedText = buildAnnotatedString {
+                withStyle(style = spanStyleGray){
+                    append(Descriptions.NEED_REGISTER)
+                }
+
+                withStyle(style = spanStyleAccent) {
+                    append(Descriptions.NEED_REGISTER_CLICKABLE)
+                }
+            }
+
+            ClickableText(
+                onClick ={ offset ->
+                    if (offset >= 16){
+                        navController.navigate(Destinations.REGISTRATION_FIRST_SCREEN)
+                    }
+                },
+                text = highlightedText
+            )
+        }
+    }
 }
