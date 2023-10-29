53d84c0cc633d04cb1ea67d0f14573990b2c917d
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2023-10-30 00:33:38 +0700

    Верстка экрана Любимое
    Полностью сверстан экран любимое. Когда экран пустой - открывается экран заглушка, иначе показывается список карточек с фильмами, чередуясь по 2 и 1 в нечетном и четном рядах соответственно

--- app/src/main/java/com/example/moviecatalog/presentation/screen/favouritescreen/FavouriteScreen.kt
+++ app/src/main/java/com/example/moviecatalog/presentation/screen/favouritescreen/FavouriteScreen.kt
@@ -1,18 +1,19 @@
 package com.example.moviecatalog.presentation.screen.favouritescreen
 
-import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -20,11 +21,22 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.moviecatalog.R
-import com.example.moviecatalog.presentation.navigation.bottombar.Routes
+import com.example.moviecatalog.domain.model.movie.MovieElement
+import com.example.moviecatalog.presentation.screen.favouritescreen.components.MovieCardFavourite
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavouriteScreen() {
+    val movie1 = MovieElement(
+        id = "1",
+        name = "Movie 1",
+        poster = "url_to_poster_1",
+        year = 2022,
+        country = "USA",
+        genres = null,
+        reviews = null
+    )
+
+    val itemsList: List<MovieElement> = listOf()
 
     Column(
         modifier = Modifier
@@ -39,9 +51,77 @@ fun FavouriteScreen() {
             modifier = Modifier.padding(bottom = 16.dp)
         )
 
-        LazyColumn {
-            items(100) { index ->
-                Text(text = "Item $index", fontSize = 16.sp, modifier = Modifier.padding(16.dp))
+        if (itemsList.isEmpty()) {
+            Spacer(modifier = Modifier.height(64.dp))
+            EmptyFavouriteScreen()
+        }
+
+        else {
+            LazyColumn {
+                item {
+                    val chunkedItems = itemsList.chunked(3)
+                    chunkedItems.forEachIndexed { index, chunk ->
+                        when (chunk.size) {
+                            3 -> {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(vertical = 8.dp),
+                                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                                ) {
+                                    MovieCardFavourite(
+                                        movie = chunk[0],
+                                        modifier = Modifier.weight(1f)
+                                    )
+                                    MovieCardFavourite(
+                                        movie = chunk[1],
+                                        modifier = Modifier.weight(1f)
+                                    )
+                                }
+
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(vertical = 8.dp),
+                                ) {
+                                    MovieCardFavourite(
+                                        movie = chunk[2],
+                                        modifier = Modifier.weight(1f)
+                                    )
+                                }
+                            }
+                            2 -> {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(vertical = 8.dp),
+                                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                                ) {
+                                    MovieCardFavourite(
+                                        movie = chunk[0],
+                                        modifier = Modifier.weight(1f)
+                                    )
+                                    MovieCardFavourite(
+                                        movie = chunk[1],
+                                        modifier = Modifier.weight(1f)
+                                    )
+                                }
+                            }
+                            else -> {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(vertical = 8.dp),
+                                ) {
+                                    MovieCardFavourite(
+                                        movie = chunk[0],
+                                        modifier = Modifier.weight(1f),
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
