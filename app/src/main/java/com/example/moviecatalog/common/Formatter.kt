ef58bb24a716c7af54893ac0c8f1a2e494e4c884
Aleksandr Kirsanov <alex.kirsanov182@gmail.com>
2023-11-15 14:50:59 +0700

    Теперь нельзя выбрать дату позднее сегодняшней

--- app/src/main/java/com/example/moviecatalog/common/Formatter.kt
+++ app/src/main/java/com/example/moviecatalog/common/Formatter.kt
@@ -1,6 +1,7 @@
 package com.example.moviecatalog.common
 
 import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
 import android.icu.util.TimeZone
 import java.time.ZoneId
 import java.util.Date
@@ -24,7 +25,11 @@ object Formatter {
     fun formatDate(date: Date?): String {
         val formatter = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
         return if (date != null) {
-            val utilDate = Date.from(date.toInstant().atZone(ZoneId.systemDefault()).toInstant())
+            val utilDate = if (date > Calendar.getInstance().time) {
+                Calendar.getInstance().time
+            } else {
+                date
+            }
             formatter.format(utilDate)
         } else {
             Constants.EMPTY_STRING
@@ -34,12 +39,18 @@ object Formatter {
     fun formatDateToISO8601(date: Date?): String {
         val formatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
         return if (date != null) {
+            val utilDate = if (date > Calendar.getInstance().time) {
+                Calendar.getInstance().time
+            } else {
+                date
+            }
             formatter.timeZone = TimeZone.getTimeZone("UTC")
-            formatter.format(date)
+            formatter.format(utilDate)
         } else {
             Constants.EMPTY_STRING
         }
     }
+
 
     fun formatDateToNormal(inputDate: String): String {
         val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault())
